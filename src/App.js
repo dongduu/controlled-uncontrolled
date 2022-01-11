cd014f4ab7b9570184785a338f0140dc53857fbc
dongduu <4012popo@naver.com>
2022-01-11 15:46:56 +0900

    feat: create uncontrolled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ControlledComponent from "./components/ControlledComponent";
+import UncontrolledCoponent from "./components/UncontrolledComponent";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ControlledComponent />
+        <UncontrolledCoponent />
       </header>
     </div>
   );
---
+++ src/components/UncontrolledComponent.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class UncontrolledCoponent extends React.Component {
+  inputRef = React.createRef();
+
+  render() {
+    console.log("initial render", this.inputRef);
+    return (
+      <div>
+        <input ref={this.inputRef} />
+        <button onClick={this.click}>전송</button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount", this.inputRef);
+  }
+
+  click = () => {
+    // 지양하는 방식
+    // const input = document.querySelector("#my-input");
+    // console.log(input.value);
+
+    console.log(this.inputRef.current.value);
+  };
+}
+
+export default UncontrolledCoponent;
